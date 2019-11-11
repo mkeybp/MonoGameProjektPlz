e636170b610eb4c862e79b4ade5db395f1cc7f1a
mikk066h <mikk066h@student.eadania.dk>
2019-11-11 12:33:50 +0100

    Class

---
+++ MonoGameProjekt/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGameProjekt
+{
+    class Bullet
+    {
+        private Vector2 position;
+        private Vector2 direction;
+        private int speed = 100;
+        private int radius = 15;
+        bool projectileFlying = false;
+
+
+        private void UpdateProjectile()
+        {
+            if (projectileFlying)
+            {
+
+            }
+        }
+    }
+}
