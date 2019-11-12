b48fe111aa0017450c754724efe020483d53a4a8
Andreas Bonde Haupt <andr62c9@student.eadania.dk>
2019-11-12 11:30:25 +0100

    Player and enemy basics

--- MonoGameProjekt/GameObject.cs
+++ MonoGameProjekt/GameObject.cs
@@ -21,11 +21,18 @@ namespace MonoGameProjekt
 
         protected Texture2D sprite;
 
+        protected Vector2 origin;
+
+        protected float rotation;
+
         public abstract void LoadContent(ContentManager content);
 
         public abstract void Update(GameTime gameTime);
 
-
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(sprite, position, null, Color.White, rotation, origin, 1, SpriteEffects.None, 0);
+        }
 
     }
 }
