2a0702deaf9ef9f296954b3870666ab16b78becf
mikk066h <mikk066h@student.eadania.dk>
2019-11-13 10:10:31 +0100

    More col

--- MonoGameProjekt/GameObject.cs
+++ MonoGameProjekt/GameObject.cs
@@ -24,14 +24,64 @@ namespace MonoGameProjekt
         protected Vector2 origin;
 
         protected float rotation;
+        protected int scaleFactor = 1;
+        protected Color rendercolor = new Color(255, 255, 255, 255);
+
+        protected Rectangle top;
+        protected Rectangle bottom;
+        protected Rectangle right;
+        protected Rectangle left;
+
+        protected int collisionOffset = 0;
+
+        public Texture2D Sprite { get => sprite; set => sprite = value;  }
+        public Color RenderColor { get => rendercolor; set => rendercolor = value; }
+
+        public Vector2 Position { get => position; set => position = value; }
+        public Vector2 Middle
+        {
+            get
+            {
+                Vector2 mid = position;
+                mid.X += Width / 2;
+                mid.Y += Height / 2;
+                return mid;
+            }
+        }
+
+        public int ScaleFactor { get => scaleFactor; set => scaleFactor = value; }
 
         public abstract void LoadContent(ContentManager content);
 
         public abstract void Update(GameTime gameTime);
 
+        public virtual Rectangle CollisionBox
+        {
+            get
+            {
+                return new Rectangle((int)position.X, (int)position.Y, sprite.Width * (int)scaleFactor, sprite.Height * (int)scaleFactor);
+            }
+        }
+
+
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(sprite, position, null, Color.White, rotation, origin, 1, SpriteEffects.None, 0);
+        }
+
+        public virtual int Width
+        {
+            get
+            {
+                return CollisionBox.Width;
+            }
+        }
+        public virtual int Height
+        {
+            get
+            {
+                return CollisionBox.Height;
+            }
         }
 
     }
